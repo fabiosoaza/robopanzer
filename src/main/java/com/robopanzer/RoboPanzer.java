22a32cb7069f00820d5b5306626e6892cb6f2890
Fábio Souza <fabio.souza.eti@gmail.com>
2018-08-26 22:18:30 -0300

    Setup of project

---
+++ src/main/java/com/robopanzer/RoboPanzer.java
@@ -0,0 +1,19 @@
+package com.robopanzer;
+
+import robocode.*;
+
+public class RoboPanzer extends Robot {
+    public void run() {
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+ 
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+}
+  
